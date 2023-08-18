00c34c3d962c9fd1ec4be4c32633e55731122e0c
aayushchauhan10 <ai.aayush10@gmail.com>
2023-08-18 14:20:51 +0530

    First Commit

---
+++ src/components/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as Industries } from "./Industries";
+export { default as Solutions } from "./Solutions";
+export { default as Products } from "./Products";
